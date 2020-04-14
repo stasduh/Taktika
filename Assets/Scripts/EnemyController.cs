216550dc2c5238fd8e2ec0cfcbb48aa62bb8a73e
Stanislav Dukhno <stasduh1984@gmail.com>
2020-04-14 21:20:38 +0300

    release v1

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    private Transform target;
+    private NavMeshAgent navComponent;
+
+    public int damage;
+    public int xp;
+    public int cost;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        navComponent = gameObject.GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navComponent.SetDestination(target.position);   
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject enemy;
+    private float nextSpawnTime;
+    public float spawnDelay = 5;
+    private Transform[] spawnArray = new Transform[3];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] allSpawn = GameObject.FindGameObjectsWithTag("Spawn");
+
+        for (int i = 0; i <= allSpawn.Length - 1; i++)
+        {
+            spawnArray[i] = allSpawn[i].transform; 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ShouldSpawn())
+        {
+            Spawn();
+        }   
+    }
+
+    private void Spawn()
+    {
+        nextSpawnTime = Time.time + spawnDelay;
+        System.Random random = new System.Random();
+        int i = random.Next(0, 2); 
+        Instantiate(enemy, spawnArray[i].transform.position, spawnArray[i].transform.rotation);
+    }
+
+    private bool ShouldSpawn()
+    {
+        return Time.time > nextSpawnTime;
+    }
+}
